d269bfec1b6a677523c40da5a02382ef3050a9d1
S-Katie <soe.kaythi38@student.qcc.cuny.edu>
2025-10-03 06:15:23 -0400

    lab_7 complete

--- lab7/lab7_script.js
+++ lab7/lab7_script.js
@@ -38,6 +38,7 @@ btn_square.addEventListener("click", function(){
     shape.className = "square"
     shape.textContent = "Square".toUpperCase()
     shape.style.backgroundColor = "gold"
+    shape.style.fontSize = "2rem"
 })
 
 btn_circle.addEventListener("click", function(){
@@ -51,4 +52,84 @@ btn_rectangle.addEventListener("click", function(){
     shape.className = "rectangle"
     shape.textContent = "Rectangle".toUpperCase()
     shape.style.backgroundColor = "coral"
+    shape.style.fontSize = "2rem"
 })
+
+// sep 30 2025
+// event handler
+
+let btnpress = document.querySelector(".btnpress")
+btnpress.onclick = function(){alert(`function()${new Date()}`)}
+
+
+// mouse events
+let x = document.querySelector(".x")
+let circle_paragraph = document.querySelector(".circle_paragraph")
+x.onmouseover = function(){circle_paragraph.textContent += "- CIRCLE -"}
+
+// event listener
+// collect the button element
+let box_color = document.querySelector(".box_color")
+// function to randomly pick a color as rgb format
+const randomRGB = function(){
+    let red = Math.floor(Math.random()*255)
+    let green = Math.floor(Math.random()*255)
+    let blue = Math.floor(Math.random()*255)
+    return `rgb(${red}, ${green}, ${blue})`
+}
+box_color.addEventListener("mouseout", function(){
+    box_color.style.backgroundColor = randomRGB()
+})
+
+// keyboard event
+let input_text = document.querySelector(".input_text")
+let username_paragraph = document.querySelector(".username_paragraph")
+input_text.addEventListener("keydown", function(){
+    username_paragraph.textContent = `Username must have at least 3 characters`
+})
+
+// LAB Exercise
+// EXERCISE
+// note: use querySelector() or querySelectorAll()
+// collect elements: search for the first p with class ps1
+// add a mouseout event to change the text content to "STUDENT'S FULL NAME"
+
+
+// search for the second p with class ps2
+// add a click event to the ps2 to change the font-size to 25px  and color to olive when is clicked
+
+
+// search for the first div with class divs2
+// add a click event to change the background color to indigo
+
+
+// search for the second div with class divs3
+// add a keydown event to change the width of divs3 to 300px
+
+
+// use querySelector() or querySelectorAll()
+// collect elements: search for the first p with class ps1
+// add a mouseout event to change the text content to "STUDENT'S FULL NAME"
+
+
+let ps1 = document.querySelector(".ps1")
+ps1.addEventListener("mouseout", function(){
+    ps1.textContent = "SOE KAYTHI"
+})
+
+let ps2 = document.querySelector(".ps2")
+ps2.addEventListener("click", function(){
+    ps2.style.fontSize = "25px"
+    ps2.style.color = "olive"
+})
+
+let divs2 = document.querySelector(".divs2")
+divs2.addEventListener("click", function(){
+    divs2.style.backgroundColor = "indigo"
+})
+
+let divs3 = document.querySelector(".divs3")
+divs3.addEventListener("keydown", function(){
+    divs3.style.width = "300px"
+})
+
