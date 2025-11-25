fb6f9deae708ee2cab5962ec6299a0c3f625f88a
soe-student <soe.kaythi38@student.qcc.cuny.edu>
2025-11-25 21:12:01 +0000

    lab16_incomplete

---
+++ lab16_shoppingcart/shoppingcart/src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+import React, {useState} from 'react';
+import ProductList from './components/productlist';
+import Cart from './components/cart';
+
+function App() {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product) => {
+    const existingItem = cart.find((item) => item.id === product.id);
+    if (existingItem) {
+      setCart(
+        cart.map((item) =>
+          item.id === product.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        )
+      );
+    } else {
+      setCart([...cart, { ...product, quantity: 1 }]);
+    }
+  }
+  return (
+    <div className="App">
+      <h1 className='apptitle'>Welcome to Soe Kaythi's supermarket</h1> 
+    
+      <ProductList addToCart = {addToCart} />
+      <Cart cart = {cart} />
+    
+    </div>
+  );
+}
+
+export default App;
